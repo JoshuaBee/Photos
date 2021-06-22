d8c8a7d2000ff6ee461053d6609f6fb0fda93d77
Joshua Bee <joshuaregular@gmail.com>
2021-06-22 21:06:24 +0100

    Updating existing photos

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,129 @@
+const orientations = {
+	PORTRAIT: 1,
+	LANDSCAPE: 2
+};
+Object.freeze(orientations);
+
 const photos = [
 	{
-		"date": "20th June 2021",
-		"file": "images/photos/2021/06/20/1000.jpg",
-		"orientation": "landscape",
+		'date': 'June 20, 2021',
+		'description': 'Annabelle at East Park visiting the wallabies',
+		'file': '2021/06/20/1000.jpg',
+		'orientation': orientations.LANDSCAPE,
 	},
 	{
-		"date": "10th June 2021",
-		"file": "images/photos/2021/06/10/1000.jpg",
-		"orientation": "portrait",
+		'date': 'June 10, 2021',
+		'description': 'Annabelle and Daddy selfie',
+		'file': '2021/06/10/1000.jpg',
+		'orientation': orientations.PORTRAIT,
 	},
 	{
-		"date": "3rd June 2021",
-		"file": "images/photos/2021/06/03/1000.jpg",
-		"orientation": "portrait",
+		'date': 'June 3, 2021',
+		'description': 'Yoghurt hair selfie',
+		'file': '2021/06/03/1000.jpg',
+		'orientation': orientations.PORTRAIT,
 	},
 	{
-		"date": "31st May 2021",
-		"file": "images/photos/2021/05/31/1000.jpg",
-		"orientation": "portrait",
+		'date': 'May 31, 2021',
+		'description': 'Annabelle phone home!',
+		'file': '2021/05/31/1000.jpg',
+		'orientation': orientations.PORTRAIT,
 	},
 	{
-		"date": "26th May 2021",
-		"file": "images/photos/2021/05/26/1000.jpg",
-		"orientation": "portrait",
+		'date': 'May 26, 2021',
+		'description': 'Learning about yoga with Llamaste',
+		'file': '2021/05/26/1000.jpg',
+		'orientation': orientations.PORTRAIT,
 	},
 	{
-		"date": "24th May 2021",
-		"file": "images/photos/2021/05/24/1000.jpg",
-		"orientation": "portrait",
+		'date': 'May 24, 2021',
+		'description': 'Getting stuck under the cot',
+		'file': '2021/05/24/1000.jpg',
+		'orientation': orientations.PORTRAIT,
 	},
 	{
-		"date": "17th May 2021",
-		"file": "images/photos/2021/05/17/1000.jpg",
-		"orientation": "portrait",
+		'date': 'May 17, 2021',
+		'description': 'Annabelle and Daddy selfie',
+		'file': '2021/05/17/1000.jpg',
+		'orientation': orientations.PORTRAIT,
 	},
 	{
-		"date": "8th May 2021",
-		"file": "images/photos/2021/05/08/1000.jpg",
-		"orientation": "landscape",
+		'date': 'May 8, 2021',
+		'description': 'Colouring on our new desk',
+		'file': '2021/05/08/1000.jpg',
+		'orientation': orientations.LANDSCAPE,
+	},
+	{
+		'date': 'April 18, 2021',
+		'description': 'Relaxing at the park',
+		'file': '2021/04/18/1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'April 17, 2021',
+		'description': 'Seaside selfie',
+		'file': '2021/04/17/1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'April 3, 2021',
+		'description': 'Reading and blanket time',
+		'file': '2021/04/03/1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'March 10, 2021',
+		'description': 'Annabelle with a big bump on her head',
+		'file': '2021/03/10/1-1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'March 10, 2021',
+		'description': 'Annabelle playing perfectly well with a big bump on her head',
+		'file': '2021/03/10/2-1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'February 27, 2021',
+		'description': 'Seaside expedition',
+		'file': '2021/02/27/1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'February 25, 2021',
+		'description': 'Reading a wedding invitation by herself',
+		'file': '2021/02/25/1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'February 17, 2021',
+		'description': 'Playing on daddy\'s shoulders',
+		'file': '2021/02/17/1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'February 14, 2021',
+		'description': 'Colouring on an envelope',
+		'file': '2021/02/14/1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'February 9, 2021',
+		'description': 'Snow day selfie',
+		'file': '2021/02/09/1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'February 6, 2021',
+		'description': 'Getting trollied',
+		'file': '2021/02/06/1000.jpg',
+		'orientation': orientations.PORTRAIT,
+	},
+	{
+		'date': 'February 2, 2021',
+		'description': 'Hanging out with Bertie Bloomer in the kitchen',
+		'file': '2021/02/02/1000.jpg',
+		'orientation': orientations.PORTRAIT,
 	},
 ];
 
@@ -104,12 +190,13 @@ function createPhotoElement(photo) {
 	const $gridItem = document.createElement('div');
 	$gridItem.ariaHidden = true;
 	$gridItem.classList.add('grid-item');
-	if (photo.orientation === "landscape") {
-		$gridItem.dataset.width = "2";
+	if (photo.orientation === orientations.LANDSCAPE) {
+		$gridItem.dataset.width = '2';
 	}
 
 	const $gridItemPhoto = document.createElement('img');
-	$gridItemPhoto.src = photo.file;
+	$gridItemPhoto.src = `images/photos/${ photo.file }`;
+	$gridItemPhoto.alt = photo.description;
 	$gridItemPhoto.classList.add('grid-item-photo');
 	$gridItem.appendChild($gridItemPhoto);
 
@@ -119,7 +206,7 @@ function createPhotoElement(photo) {
 
 	const $photoOverlayText = document.createElement('div');
 	$photoOverlayText.classList.add('photo-overlay-text');
-	$photoOverlayText.innerHTML = photo.date;
+	$photoOverlayText.innerHTML = new Date(photo.date).toDateString();
 	$photoOverlay.appendChild($photoOverlayText);
 
 	//$grid.appendChild($gridItem);
